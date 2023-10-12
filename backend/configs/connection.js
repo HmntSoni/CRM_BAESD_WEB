a25654078388c6488094f032f4ae3d895970211f
abhijeetsenguptaa <101327881+abhijeetsenguptaa@users.noreply.github.com>
2023-10-12 12:21:31 +0530

    added some more basics

--- backend/configs/connection.js
+++ backend/configs/connection.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 
-class Database { 
-    
+class Database {
+    constructor() {
+        this.connection = mongoose.connect(process.env.mongoURL)
+    }
 }
+
+module.exports = new Database
--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,36 @@
-require('dotenv').config();
+require('dotenv').config();  // Load environment variables from a .env file if present
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const connection = require('./configs/connection');  // Import your database connection setup
 
 class Server {
     constructor() {
         this.PORT = process.env.PORT || 7001;
         this.app = express();
 
-        this.setMiddleware();
-        this.startServer();
+        this.setMiddleware();  // Set up middleware
+        this.startServer();  // Start the server
     }
 
     setMiddleware() {
-        this.app.use(express.json());
-        this.app.use(cors());
-        this.app.use(cookieParser());
+        this.app.use(express.json());  // Parse JSON request bodies
+        this.app.use(cors());  // Enable CORS
+        this.app.use(cookieParser());  // Parse cookies in the request
     }
 
-    startServer() {
-        this.app.listen(this.PORT, () => {
-            console.log(`Server is running on port ${this.PORT}`);
-        });
+    async startServer() {
+        try {
+            await connection;  // Connect to the database
+            console.log('Server is connected to the DB.');
+            this.app.listen(this.PORT, () => {
+                console.log(`Server is running on port ${this.PORT}`);
+            });
+        } catch (error) {
+            console.error('Server could not get connected to the DB.');
+            process.exit(1); // Exit the application on database connection failure
+        }
     }
 }
 
-new Server();
+new Server();  // Create an instance of the Server class to start the server
